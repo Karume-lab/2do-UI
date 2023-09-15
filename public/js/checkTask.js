b0f3091ec990ff9389f5688434985a6868f6bcfb
Karume-lab <danielkarume@outlook.com>
2023-09-15 14:05:35 +0300

    implemented progress bar

--- public/js/checkTask.js
+++ public/js/checkTask.js
@@ -1,10 +1,11 @@
 const checkTask = (taskId) => {
 	const checkbox = document.querySelector(`#markTask${taskId}`);
 	const checkVal = checkbox.checked;
-	const task = taskArray[taskId];
+	const task = taskArray[taskId - 1];
 	checkVal ? task.checked = checkVal : task.checked = false;
 	const tskId = "task" + taskId;
 	const taskBtn = document.querySelector(`#${tskId}`);
 	taskBtn.classList.toggle("line-through");
 	localStorage.setItem("tasks", JSON.stringify(taskArray));
+	location.reload();
 }
